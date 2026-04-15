38eef767cf774e3a2acd62d468c5834da01692ca
nlsa8750 <nlsa8750@gmail.com>
2026-04-15 08:45:10 +0530

    Initial Commit

---
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/CodeLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.Technick_Code.code_learning_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeLearningPlatformApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Technick_Code/code_learning_platform/CodeLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Technick_Code.code_learning_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
