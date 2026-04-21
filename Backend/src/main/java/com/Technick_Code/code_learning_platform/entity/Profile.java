d220a968721ea035c29afbf35d6f27103da3b76c
nlsa8750 <nlsa8750@gmail.com>
2026-04-21 16:31:28 +0530

    Added Profile model file

---
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Profile.java
@@ -0,0 +1,57 @@
+package com.Technick_Code.code_learning_platform.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "profiles")
+public class Profile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(columnDefinition = "TEXT", name = "avatar_url")
+    private String avatarUrl;
+
+    @Column(columnDefinition = "TEXT")
+    private String bio;
+
+    @Column(columnDefinition = "TEXT", name = "github_url")
+    private String githubUrl;
+
+    @Column(columnDefinition = "TEXT", name = "linkedin_url")
+    private String linkedinUrl;
+
+    // integer kyuki nullable hai
+    @Column(name = "current_streak")
+    private Integer currentStreak;
+
+    @Column(name = "max_streak")
+    private Integer maxStreak;
+
+    @Column(name = "last_active_date")
+    private LocalDate lastActiveDate;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;  
+
+
+}
