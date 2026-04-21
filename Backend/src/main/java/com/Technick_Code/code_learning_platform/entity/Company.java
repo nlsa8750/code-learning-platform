d36a5e472b657a9a39b758dce098274b5ffaa5b8
nlsa8750 <nlsa8750@gmail.com>
2026-04-21 17:48:56 +0530

    Added Company Model File

---
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Company.java
@@ -0,0 +1,26 @@
+package com.Technick_Code.code_learning_platform.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "companies")
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String logoUrl;
+
+}
