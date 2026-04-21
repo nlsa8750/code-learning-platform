5199f2b163df8cf3a32b734c8499950eb66ab22b
nlsa8750 <nlsa8750@gmail.com>
2026-04-21 14:52:26 +0530

    Added SecurityConfig file

---
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.Technick_Code.code_learning_platform.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+@EnableWebSecurity 
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+            );
+            return http.build();
+    }
+
+}
+
+
