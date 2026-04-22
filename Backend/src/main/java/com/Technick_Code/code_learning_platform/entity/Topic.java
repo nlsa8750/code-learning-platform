a2e10496b207c58e04be6e10a9cd4d21d6c93631
nlsa8750 <nlsa8750@gmail.com>
2026-04-22 11:48:40 +0530

    Change the data type to OffsetDateTime

--- Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Topic.java
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Topic.java
@@ -6,28 +6,32 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import java.time.LocalDateTime;
+import lombok.NoArgsConstructor;
+import java.time.OffsetDateTime;
 
 @Data
 @Entity
 @Table(name = "topics")
+@NoArgsConstructor // for jpa at the creation of object
+@AllArgsConstructor // while creating object manually 
 public class Topic {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(nullable =  false, unique = true)
+    @Column(nullable =  false, unique = true, length = 100)
     private String name;
 
-    @Column(nullable =  false, unique = true)
+    @Column(nullable =  false, unique = true, length = 100)
     private String slug;
 
     @Column(columnDefinition = "TEXT")
     private String description;
 
     @Column(name = "created_at")
-    private LocalDateTime createdAt;
+    private OffsetDateTime createdAt;
 
 }
