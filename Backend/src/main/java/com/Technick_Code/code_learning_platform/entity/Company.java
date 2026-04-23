f127fa5ad4b4f30cdcbd367869e88827ff90233d
nlsa8750 <nlsa8750@gmail.com>
2026-04-23 17:53:14 +0530

    Change int to Integer to handle null constraints

--- Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Company.java
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/Company.java
@@ -6,19 +6,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "companies")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Company {
 
     // auto-increment using generation type identity
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, unique = true, length = 100)
     private String name;
 
     @Column(columnDefinition = "TEXT", name = "logo_url")
