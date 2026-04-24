3ab6933ab256d8e0b770477fd4fbc94507df1022
nlsa8750 <nlsa8750@gmail.com>
2026-04-24 10:13:52 +0530

    Change data type to handle null constraints

--- Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/StreakHistory.java
+++ Backend/src/main/java/com/Technick_Code/code_learning_platform/entity/StreakHistory.java
@@ -1,24 +1,26 @@
 package com.Technick_Code.code_learning_platform.entity;
 
-import java.util.UUID;
-
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
 @Data
 @Entity
 @Table(name = "streak_history")
-
+@AllArgsConstructor
+@NoArgsConstructor
 public class StreakHistory {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Integer id;
 
-    // user_id = profiles.id se link kar raha hai
-    @Column(name = "user_id")
-    private UUID userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "user_id", nullable = false)
+    private Profile user;
 
     @Column(nullable = false)
     private LocalDate date;
